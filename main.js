0e40238e26efc0e3ba5ec556a224d1da9b474b9a
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-06 22:11:17 -0500

    paste firebase credentials in main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+//firebase credentials
+const config = {
+  apiKey: "AIzaSyBnduQWcU_8ArMqPKW_AiYK2g2x-UdQFBc",
+  authDomain: "project-one-bbbef.firebaseapp.com",
+  databaseURL: "https://project-one-bbbef-default-rtdb.firebaseio.com",
+  projectId: "project-one-bbbef",
+  storageBucket: "project-one-bbbef.appspot.com",
+  messagingSenderId: "57284089155",
+  appId: "1:57284089155:web:f1a0159e1915e174938bf6",
+  measurementId: "G-LW4X4W1F55"
+};
+
+firebase.initializeApp(config);
